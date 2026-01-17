ae90eeec56be2481caa6da8a928b8b4b03e71c73
Pufa069 <110808879+Pufa069@users.noreply.github.com>
2026-01-17 13:37:10 +0700

    fix withReactContent not defined

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import Select from "react-select";
 import axios from "axios";
 import "../App.css";
