3b1623ab23f04763026af6918bf6184fd55f7830
Pufa069 <110808879+Pufa069@users.noreply.github.com>
2026-01-17 21:49:07 +0700

    update format image

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,8 @@ const uploadToCloudinary = (buffer) => {
     const stream = cloudinary.uploader.upload_stream(
       {
         folder: "menus",
-        width: 500,
-        height: 500,
+        width: 400,
+        height: 400,
         crop: "fill",
         gravity: "center",
         quality: "auto",
